108020167e5bd91da5764fe6235edb863ceafd06
Mathias Bruun <mathiasbruun.mb@gmail.com>
2023-07-24 21:27:25 +0200

    fix naming inconsistency

--- DownloadTranscripts.py
+++ DownloadTranscripts.py
@@ -6,7 +6,7 @@ daterange = pd.bdate_range(datetime(2020,1,29), datetime(2020,1,30))
 
 def download_senate_pdf(date):
     try:
-        url = f'https://www.congress.gov/116/crec/{single_date.year}/{single_date.month:02d}/{single_date.day:02d}/CREC-{single_date.year}-{single_date.month:02d}-{single_date.day:02d}.pdf'
+        url = f'https://www.congress.gov/116/crec/{date.year}/{date.month:02d}/{date.day:02d}/CREC-{date.year}-{date.month:02d}-{date.day:02d}.pdf'
         r = requests.get(url, allow_redirects=True)
         r.raise_for_status()
         open(f'transcripts-pdf/{date.strftime("%Y-%m-%d")}.pdf', 'wb').write(r.content)
